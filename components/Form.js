824bf35c25e96eb57070217933d0168c6fffc807
jai <jai.bhalla2021@vitstudent.ac.in>
2023-01-02 20:33:21 +0530

    Adding the conditional rendering of the form

--- components/Form.js
+++ components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = (themes) => {
     return (
-        <div className={`${themes.themes?"bg-gray-200":"bg-gray-800"}` + ' mt-5 mb-10 p-10 border-none rounded-lg'}>
+        <div className={`${themes.themes?"bg-gray-100":"bg-gray-700"}` + ' mt-5 p-10 border-none rounded-lg'}>
             This is the form portion
         </div>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,14 @@ import {useState} from "react";
 
 export default function Home() {
     const [theme, setTheme] = useState(true);
+    const [visible, setVisible] = useState(false);
+
     const handleChange = ()=>{
         setTheme(current=>!current)
+    }
+
+    const handleClick = ()=>{
+        setVisible(current=>!current)
     }
 
   return (
@@ -19,15 +25,24 @@ export default function Home() {
       <main className='m-0 p-10'>
           <h1 className={`${theme?"text-black":"text-white"}` + ' text-5xl font-light flex justify-center'}>Student Management Application</h1>
           <div>
-              <div className={`${theme?"bg-gray-200":"bg-gray-800"}` + ' flex justify-between mt-5 mb-10 p-10 border-none rounded-lg'}>
-                  <Button variant={'contained'} color={'success'} className={'bg-green-700'}>Add Student <AiOutlineUserAdd size={20}/> </Button>
-                  <div>
-                      <Switch
-                        onChange={handleChange}
-                      />
+              <div className={`${theme?"bg-gray-200":"bg-gray-800"}` + ' mt-5 mb-10 p-10 border-none rounded-lg'}>
+                  <div className={'flex justify-between'}>
+                      <Button
+                          variant={'contained'}
+                          color={'success'}
+                          className={'bg-green-700'}
+                          onClick={handleClick}
+                      >Add Student <AiOutlineUserAdd size={20}/> </Button>
+                      <div className={`${theme?"text-black":"text-white"}` + ' font-light text-lg'}>
+                          Change Theme
+                          <Switch
+                              color={'default'}
+                              onChange={handleChange}
+                          />
+                      </div>
                   </div>
+                  {visible && (<Form themes={theme}/>)}
               </div>
-              {<Form themes={theme}/>}
               <Table themes={theme}/>
           </div>
       </main>
