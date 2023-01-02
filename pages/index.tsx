272eaac638d165d2b2509ae0c62287f627bf80d1
jai <jai.bhalla2021@vitstudent.ac.in>
2023-01-02 18:17:20 +0530

    Adding the tailwind-css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Student Management App</title>
       </Head>
-      <main>
+      <main className='bg-red-200'>
+          <div className={'text-3xl'}>
+              this is for testing the tailwind css
+          </div>
         Hye this will be a student management app😉😊😊😊😊
       </main>
     </div>
