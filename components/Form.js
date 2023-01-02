ec759f24b4f2cc1811f7bcfbcd47d778950ce02f
jai <jai.bhalla2021@vitstudent.ac.in>
2023-01-02 21:21:08 +0530

    Making the stucture for the form

--- components/Form.js
+++ components/Form.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import {Button, TextField} from "@mui/material";
+
 
 const Form = (themes) => {
+
     return (
         <div className={`${themes.themes?"bg-gray-100":"bg-gray-700"}` + ' mt-5 p-10 border-none rounded-lg'}>
-            This is the form portion
+            <h1 className={`${themes.themes?"text-black":"text-white"}` + ' text-3xl text-center mb-10'}>Add Student</h1>
+            <form className={'grid lg:grid-cols-2 gap-10'}>
+                <TextField
+                    id="outlined-textarea"
+                    label="Name"
+                    color={'primary'}
+                />
+                <TextField
+                    id="outlined-textarea"
+                    label="Age"
+                    type={'number'}
+                    color={'primary'}
+                />
+                <TextField
+                    id="outlined-textarea"
+                    label="Phone Number"
+                    type={'number'}
+                    color={'primary'}
+                />
+                <TextField
+                    id="outlined-textarea"
+                    label="Email"
+                    color={'primary'}
+                />
+                <TextField
+                    id="outlined-textarea"
+                    label="Address"
+                    color={'primary'}
+                />
+                <TextField
+                    id="outlined-textarea"
+                    label="Image-Url"
+                    color={'primary'}
+                />
+                <Button
+                    className={'grid col-span-2 w-40 bg-blue-600 m-auto'}
+                    variant={'contained'}
+                >Submit</Button>
+            </form>
+
         </div>
     );
 };
