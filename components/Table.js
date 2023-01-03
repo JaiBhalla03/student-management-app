b70690fe9f0bef182410a89540ea8e85d185764e
jai <jai.bhalla2021@vitstudent.ac.in>
2023-01-03 23:06:57 +0530

    Customizing the table

--- components/Table.js
+++ components/Table.js
@@ -1,19 +1,98 @@
 import React, {useEffect} from 'react';
 import {useQuery} from "react-query";
 import {getStudents} from "../lib/helper";
+import {createTheme} from "@mui/material";
 
 const Table = (themes) => {
-    const {data} = useQuery("students", getStudents)
+    const {data, isLoading, isError, error} = useQuery("students", getStudents)
     console.log(data)
+    if(isLoading){
+        return <div>Students are loading</div>
+    }
+    if(isError){
+        return <div>Error: {error}</div>
+    }
     return (
         <div className={`${themes.themes?"bg-gray-200":"bg-gray-800"}` + ' mt-5 mb-10 p-10 border-none rounded-lg'}>
-            {data?.map((student)=>{
-                return (
-                    <div key={student._id}>{student.name}</div>
-                )
-            })}
+            <h1 className={`${themes.themes?"":"text-white"}` + ' text-center mb-5 text-3xl font-bold'}>Students</h1>
+            <table className={`${themes.themes?"":"text-white"}` + ' container rounded-t-2xl'}>
+                <thead className={'rounded-t-2xl'}>
+                <tr className={`${themes.themes?"bg-gray-400":"bg-gray-700 text-white"}` + ' rounded-t-2xl'}>
+                    <th className={'p-2 rounded-tl-2xl'}>
+                        <span>
+                            Profile
+                        </span>
+                    </th>
+                    <th className={'p-2'}>
+                        <span>
+                            Name
+                        </span>
+                    </th>
+                    <th className={'p-2'}>
+                        <span>
+                            Age
+                        </span>
+                    </th>
+                    <th className={'p-2'}>
+                        <span>
+                            Phone Number
+                        </span>
+                    </th>
+                    <th className={'p-2'}>
+                        <span>
+                            Email
+                        </span>
+                    </th>
+                    <th className={'p-2 rounded-tr-2xl'}>
+                        <span>
+                            Action
+                        </span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody className={`${themes.themes?"":"bg-gray-600"}`}>
+                    {data.map((obj, i)=><Tr {...obj} key={i} theme={themes}/>)}
+                </tbody>
+            </table>
         </div>
     );
 };
+
+const Tr = ({imageUrl, name, age, phoneNumber, email}, theme)=>{
+    return(
+        <tr>
+            <td className={'text-center border-gray-400 border-2'}>
+                <span>
+                    <img src={imageUrl} alt={''}/>
+                </span>
+            </td>
+            <td className={'text-center p-1 text-lg border-gray-400 border-2'}>
+                <span className={''}>
+                    {name}
+                </span>
+            </td>
+            <td className={'text-center p-1 text-lg border-gray-400 border-2'}>
+                <span className={''}>
+                    {age}
+                </span>
+            </td>
+            <td className={'text-center p-1 text-lg border-gray-400 border-2'}>
+                <span>
+                    {phoneNumber}
+                </span>
+            </td>
+            <td className={'text-center p-1 text-lg border-gray-400 border-2'}>
+                <span>
+                    {email}
+                </span>
+            </td>
+            <td className={'text-center p-1 text-lg border-gray-400 border-2'}>
+                <span>
+                    Delete Update
+                </span>
+            </td>
+        </tr>
+    )
+}
 
 export default Table;
