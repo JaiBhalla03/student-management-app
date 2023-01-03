0bedae63f34fe74fbefc9ac579ee6a8466c3e735
jai <jai.bhalla2021@vitstudent.ac.in>
2023-01-03 18:05:14 +0530

    getting data to the frontend

--- components/Table.js
+++ components/Table.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useQuery} from "react-query";
+import {getStudents} from "../lib/helper";
 
 const Table = (themes) => {
+    const {data} = useQuery("students", getStudents)
+    console.log(data)
     return (
         <div className={`${themes.themes?"bg-gray-200":"bg-gray-800"}` + ' mt-5 mb-10 p-10 border-none rounded-lg'}>
-            This is the table part
+            {data.map((student)=>{
+                return (
+                    <div key={student._id}>{student.name}</div>
+                )
+            })}
         </div>
     );
 };
